786ccfad422f58f48af5621c9d0c67a6b93d8b2a
sysvisyut <sharvajvidyut@gmail.com>
2025-12-24 19:56:11 +0530

    bfs traversal

---
+++ graphs/bfs.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // code here
+        int n = adj.size();
+        vector<int> vis(n,0);
+        vis[0] = 1;
+        queue<int> q;
+        q.push(0);
+        vector<int> bfs;
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            bfs.push_back(node);
+            
+            for(auto it: adj[node]){
+                if(!vis[it]){
+                    vis[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+};
