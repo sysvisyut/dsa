c6e50749dc8e67a50bb40050652949d2d0dc5bb4
sysvisyut <sharvajvidyut@gmail.com>
2026-03-06 15:22:35 +0530

    find max depth of BT

---
+++ trees/binary_trees/depthOfBinaryTree.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int findHeight(TreeNode* node){
+        if(!node) return 0;
+
+
+        return 1+max(findHeight(node->left),findHeight(node->right));
+    }
