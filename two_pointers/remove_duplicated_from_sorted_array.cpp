67c9ac338f17914e15bbf0aad1b2a815b540793e
sysvisyut <sharvajvidyut@gmail.com>
2026-03-16 15:11:42 +0530

    created two pointers folder and added remove duplicated from sorted arrayone

---
+++ two_pointers/remove_duplicated_from_sorted_array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int l = 0;
+        for(int r=1;r<nums.size();r++){
+            if(nums[l] != nums[r]){    //found a unique element
+                l++;
+                nums[l] = nums[r];              
+            }
+        }
+
+        return l+1;
+    }
+};
