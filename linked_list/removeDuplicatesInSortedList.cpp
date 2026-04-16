b0cc1641a3fea80c938910812f08b4f83c7570cc
sysvisyut <sharvajvidyut@gmail.com>
2026-04-16 17:55:43 +0530

    remove duplicates in a sorted LL

---
+++ linked_list/removeDuplicatesInSortedList.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* temp = head;
+
+        while(temp && temp->next){
+            if(temp->val == temp->next->val){
+                temp->next = temp->next->next;
+            }
+            else{
+                temp = temp->next;
+            }
+        }
+        return head;
+    }
+};
