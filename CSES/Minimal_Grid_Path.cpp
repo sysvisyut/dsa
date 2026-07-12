a62bcd803dd57049a2bd00a86f12e72af9df4420
sysvisyut <sharvajvidyut@gmail.com>
2026-07-12 16:14:44 +0530

    minimal grid path CSES

--- CSES/Minimal_Grid_Path.cpp
+++ CSES/Minimal_Grid_Path.cpp
@@ -19,37 +19,16 @@ using namespace std;
 typedef long long ll;
 #define fastio ios::sync_with_stdio(false); cin.tie(NULL);
 
+int dx[] = {0,1};
+int dy[] = {1,0};
+int n;
 
-
-string helper(int i, int j, vector<vector<string>>& grid,int n){
-
-    
-    if(i == n-1 && j == n-1){
-        return grid[i][j];
-    }
-    string down = "";
-    string right = "";
-
-    //when we at the last row or last col, we get empty string and its always smaller so should handle it seperately
-
-    if(i == n-1){ // last row, only right is possible
-        return grid[i][j]+helper(i,j+1, grid,n);
-    }
-    if(j == n-1){ // last col, only down is possible
-        return grid[i][j]+helper(i+1,j, grid,n);
-    }
-    
-    down = grid[i][j]+ helper(i+1,j, grid, n);
-    
-    right = grid[i][j]+helper(i,j+1, grid,n);
-    return min(down, right);
-}
+string res;
 
 void solve() {
-    int n;
     cin>> n;
 
-    vector<vector<string>> grid(n, vector<string>(n));
+    vector<vector<char>> grid(n, vector<char>(n));
 
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
@@ -57,7 +36,49 @@ void solve() {
         }
     }
 
-    cout<< helper(0,0, grid,n);
+    vector<vector<bool>> vis(n,vector<bool>(n,false));
+    res ="";
+    res+=grid[0][0];
+
+    vector<pair<int,int>> currLayer;
+    currLayer.push_back({0,0});
+    
+    vis[0][0] = true;
+
+    while(res.size() < 2*n -1){
+        char best = 'Z'+1;
+        vector<pair<int,int>> nextLayer;
+         for(auto[ux, uy]: currLayer){
+            for(int i=0;i<2;i++){
+                int vx = ux+dx[i];
+                int vy = uy+dy[i];
+
+                if(vx>=n || vy>=n || vis[vx][vy] ){
+                    continue;
+                }
+
+                vis[vx][vy] = 1;
+                nextLayer.push_back({vx,vy});
+
+                if(grid[vx][vy] < best){
+                    best = grid[vx][vy];
+                }
+            }
+        }
+
+        res+=best;
+
+        vector<pair<int,int>> filteredNextLayer;
+
+        for(auto[ux,uy] : nextLayer){
+            if(grid[ux][uy] == best){
+                filteredNextLayer.push_back({ux,uy});
+            }
+        }
+
+        currLayer = move(filteredNextLayer);
+    }
+
 }
 
 int main() {
@@ -68,6 +89,8 @@ int main() {
     while(t--) {
         solve();
     }
+
+    cout<< res;
 
     return 0;
 }
