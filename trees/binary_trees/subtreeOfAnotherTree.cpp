e4678f60c704e575c6929b2bbf7037fefdff0946
sysvisyut <sharvajvidyut@gmail.com>
2026-08-31 19:45:16 +0530

    subtree of another tree

---
+++ trees/binary_trees/subtreeOfAnotherTree.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+
+}
+
+class Solution {
+public:
+
+    bool isSame(TreeNode* r, TreeNode* sr){
+
+        if(!r && !sr) return true;
+
+        if(!r || !sr) return false;
+
+
+        if(r->val != sr->val) return false;
+
+        return isSame(r->left,sr->left) && isSame(r->right,sr->right);
+        
+    }
+    
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        
+        if(!root) return false;
+        
+        if(isSame(root,subRoot)){
+            return true;
+        }
+
+        return isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
+
+    }
+};
